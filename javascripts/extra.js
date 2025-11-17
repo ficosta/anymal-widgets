6dfca990a302e04ab906427f0577d811067687f3
Felipe Iasi <felipeiasi@gmail.com>
2025-11-18 00:07:36 +0100

    Deployed e737924 with MkDocs version: 1.6.1

---
+++ javascripts/extra.js
@@ -0,0 +1,7 @@
+// Custom JavaScript para Anymal Widgets
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('Anymal Widgets Documentation loaded');
+
+  // Adicionar funcionalidades extras aqui se necessário
+});
